af4d7c0ef4aaad2abe3077d0119c4148b073775d
Your Name <you@example.com>
2017-12-04 14:43:04 +0100

    Continuacion

--- js/funcion.js
+++ js/funcion.js
@@ -1,4 +1,15 @@
-numprim = {};
-numprim.calculate = function (number) {
-
-}
+$(document).ready(function(){
+    $('#calcular').click(function(){   
+        numero = parseInt(numero); 
+        if(numero>=1 && !isNaN(numero)){
+            $("#mensajes").append("<tr><th>Números primos de "+numero);
+            for (var x =1; x<=numero; x++){
+                if(esPrimo(x)){
+                    document.write("<tr><td>"+ x +"</td></tr>");
+                }
+            }
+        }else{
+        alert("¡El valor introducido NO es válido!")
+        };
+    });
+})
---
+++ js/funcion2.js
@@ -0,0 +1,10 @@
+function esPrimo(num){
+    i=2
+    while (i<num){
+        if(num%i==0){
+            return false;
+        }
+    i++;
+    }
+    return true;
+}
